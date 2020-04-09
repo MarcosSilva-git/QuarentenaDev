9bf08ca3ec9415bbe72d173eb46751d1a5c7c1d5
MarcosSilva-git <marcosjoseoliveira.mj@gmail.com>
2020-04-08 23:01:34 -0300

    Aula1/Desafio4 resolvido

---
+++ Aula_1/Desafio_4/Desafio4.js
@@ -0,0 +1,7 @@
+raio = prompt("Qual é o raio do círculo? (cm)")
+pi = 3.14
+
+document.write("O valor da área do círculo cujo raio é ")
+document.write(raio)
+document.write("cm é igual a ")
+document.write(pi*raio*raio, "cm")
