9516d9661da4ca0b3c22f4740494456a04f0fe07
MarcosSilva-git <marcosjoseoliveira.mj@gmail.com>
2020-04-13 21:54:45 -0300

    Aula 7 concluida

---
+++ Aula_7/Desafios/Desafio1.js
@@ -0,0 +1,15 @@
+function sortear(){
+    aleatorio = Math.round(Math.random()*10)
+    console.log(aleatorio)
+    numeroSorteado = prompt("Digite seu número da sorte:")
+    if(numeroSorteado == aleatorio){
+        mensagem.innerHTML = "Você ganhou <br>o Sorteio! <br> Código:<br>"+ Math.round(Math.random()*100000000)
+        mensagem.style.color = "green"
+    } else if (numeroSorteado < 0 || numeroSorteado > 10) {
+        mensagem.innerHTML = "Digite um número <br> de 0 até 10!"
+        mensagem.style.color = "yellow"
+    } else {
+        mensagem.innerHTML = "Não foi <br> dessa vez. <br> Tente Novamente!"
+        mensagem.style.color = "red"
+    }
+}
