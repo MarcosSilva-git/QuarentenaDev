35fbd412f3d840f200152057fbceb459e6776df7
MarcosSilva-git <marcosjoseoliveira.mj@gmail.com>
2020-04-20 10:38:39 -0300

    Conclusao do Certificado!

--- Aula_Final/Certificado/Certificado.js
+++ Aula_Final/Certificado/Certificado.js
@@ -7,70 +7,36 @@ function gerar() {
     }
 
     listaLink = [
-        'https://github.com/MarcosSilva-git/QuarentenaDev/tree/master/Aula_Final/Certificado',
-        'https://github.com/MarcosSilva-git/QuarentenaDev/tree/master/Aula_9/Site_Calcular',
-        'https://github.com/MarcosSilva-git/QuarentenaDev/tree/master/Aula_9/Desafio1',
-        'https://github.com/MarcosSilva-git/QuarentenaDev/tree/master/Aula_8/Likes_Ilimitados',
-        'https://github.com/MarcosSilva-git/QuarentenaDev/tree/master/Aula_8/Likes_Limitados',
-        'https://github.com/MarcosSilva-git/QuarentenaDev/tree/master/Aula_8/Desafio4',
-        'https://github.com/MarcosSilva-git/QuarentenaDev/tree/master/Aula_8/Desafio3',
-        'https://github.com/MarcosSilva-git/QuarentenaDev/tree/master/Aula_8/Desafio2',
-        'https://github.com/MarcosSilva-git/QuarentenaDev/tree/master/Aula_8/Desafio1',
-        'https://github.com/MarcosSilva-git/QuarentenaDev/tree/master/Aula_7/Desafios',
-        'https://github.com/MarcosSilva-git/QuarentenaDev/tree/master/Aula_6/Ficha',
-        'https://github.com/MarcosSilva-git/QuarentenaDev/tree/master/Aula_6/Desafio1',
-        'https://github.com/MarcosSilva-git/QuarentenaDev/tree/master/Aula_5/Pokebola',
-        'https://github.com/MarcosSilva-git/QuarentenaDev/tree/master/Aula_5/Desafio2',
-        'https://github.com/MarcosSilva-git/QuarentenaDev/tree/master/Aula_5/Desafio1',
-        'https://github.com/MarcosSilva-git/QuarentenaDev/tree/master/Aula_5/Curr%C3%ADculo',
-        'https://github.com/MarcosSilva-git/QuarentenaDev/tree/master/Aula_4/Desafio%20%C3%9Anico',
-        'https://github.com/MarcosSilva-git/QuarentenaDev/tree/master/Aula_3/Desafio2',
-        'https://github.com/MarcosSilva-git/QuarentenaDev/tree/master/Aula_3/Desafio1',
-        'https://github.com/MarcosSilva-git/QuarentenaDev/tree/master/Aula_2/Desafio3',
-        'https://github.com/MarcosSilva-git/QuarentenaDev/tree/master/Aula_2/Desafio2',
-        'https://github.com/MarcosSilva-git/QuarentenaDev/tree/master/Aula_2/Desafio1',
-        'https://github.com/MarcosSilva-git/QuarentenaDev/tree/master/Aula_1/Desafio_4',
-        'https://github.com/MarcosSilva-git/QuarentenaDev/tree/master/Aula_1/Desafio_3',
-        'https://github.com/MarcosSilva-git/QuarentenaDev/tree/master/Aula_1/Desafio_2',
-        'https://github.com/MarcosSilva-git/QuarentenaDev/tree/master/Aula_1/Desafio_1',
-
+        'https://github.com/MarcosSilva-git/QuarentenaDev/tree/master/Aula_Final',
+        'https://github.com/MarcosSilva-git/QuarentenaDev/tree/master/Aula_9',
+        'https://github.com/MarcosSilva-git/QuarentenaDev/tree/master/Aula_8',
+        'https://github.com/MarcosSilva-git/QuarentenaDev/tree/master/Aula_7',
+        'https://github.com/MarcosSilva-git/QuarentenaDev/tree/master/Aula_6',
+        'https://github.com/MarcosSilva-git/QuarentenaDev/tree/master/Aula_5',
+        'https://github.com/MarcosSilva-git/QuarentenaDev/tree/master/Aula_4',
+        'https://github.com/MarcosSilva-git/QuarentenaDev/tree/master/Aula_3',
+        'https://github.com/MarcosSilva-git/QuarentenaDev/tree/master/Aula_2',
+        'https://github.com/MarcosSilva-git/QuarentenaDev/tree/master/Aula_1',
     ]
 
     atividade = [
-        'Aula10/Certificado',
-        'Aula9/Calculo do Uber (completo)',
-        'Aula9/Inputs no HTML',
-        'Aula8/Likes Ilimitados',
-        'Aula8/Likes Limitados',
-        'Aula8/Números aleatórios',
-        'Aula8/Contagem Pares',
-        'Aula8/Contagem até 500',
-        'Aula8/Quantidade de Olimpíadas',
-        'Aula7/Pequena Validação de Dados',
-        'Aula6/Ficha',
-        'Aula6/Meses do Ano',
-        'Aula5/Pokebola Com CSS',
-        'Aula5/Layout Reproduzido',
-        'Aula5/Estilizando a Calculadora',
-        'Aula5/Currículo',
-        'Aula4/Calculo do Uber',
-        'Aula3/Calculo da Gasolina',
-        'Aula3/Quebra de Linha',
-        'Aula2/Idade Atual',
-        'Aula2/IMC (completo)',
-        'Aula2/Média de Idade',
-        'Aula1/Área da Circunferência',
-        'Aula1/Balas Compradas',
-        'Aula1/Galinhas Compradas',
-        'Aula1/Batimentos Cardíacos',
-        
+        'Aula10 - Certificado.',
+        'Aula9 - Inputs e Cálculos da Uber (completo).',
+        'Aula8 - Like, Números Aleatórios, Pares, Contagem e Olimíadas.',
+        'Aula7 - Pequena Validação de <br>Dados.',
+        'Aula6 - Ficha e Meses do Ano.', 
+        'Aula5 - Pokebola, Layout, <br>Calculadora e Currículo.',
+        'Aula4 - Calculo do Uber.',
+        'Aula3 - Calculo da Gasolina e <br>Quenbra da Linha.',
+        'Aula2 - Idade Atual, IMC e Média.',
+        'Aula1 - Área da Circunferência, <br>Compras e Batimento Cardíaco.',        
     ]
 
     listaDeLi = ""
     contador = 0
     while (contador < 10){
         link = listaLink[contador]
-        listaDeLi = `<li><a href=${link}>${atividade[contador]}</a></li>`+listaDeLi
+        listaDeLi = `<li><a target="blank" href=${link}>${atividade[contador]}</a></li>`+listaDeLi
         contador += 1
 
     }
