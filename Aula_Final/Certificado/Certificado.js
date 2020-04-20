36e429ea52f11b5408c7e9db15d526e359d6dde6
MarcosSilva-git <marcosjoseoliveira.mj@gmail.com>
2020-04-20 03:22:37 -0300

    Aula 10 mudadda para Aula Final

---
+++ Aula_Final/Certificado/Certificado.js
@@ -0,0 +1,59 @@
+function gerar() {
+    nome = inputNome.value
+    contador = 1000000
+    while (contador > 0){
+        htmlCertificado = emitirCertificado(nome)
+        contador = contador - 1
+    }
+
+    listaLink = [
+        
+    ]
+
+    atividade = [
+        'Aula10/Certificado',
+        'Aula9/Calculo do Uber (completo)',
+        'Aula9/Inputs no HTML',
+        'Aula8/Likes Ilimitados',
+        'Aula8/Likes Limitados',
+        'Aula8/Contagem Pares',
+        'Aula8/Contagem até 500',
+        'Aula8/Quantidade de Olimpíadas',
+        'Aula7/Pequena Validação de Dados',
+        'Aula6/Ficha',
+        'Aula6/Meses do Ano',
+        'Aula5/Pokebola Com CSS',
+        'Aula5/Layout Reproduzido',
+        'Aula5/Estilizando a Calculadora',
+        'Aula5/Currículo',
+        'Aula4/Calculo do Uber',
+        'Aula3/Quebra de Linha',
+        'Aula3/Calculo da Gasolina',
+        'Aula2/Idade Atual',
+        'Aula2/IMC (completo)',
+        'Aula2/Média de Idade',
+        'Aula1/Área da Circunferência',
+        'Aula1/Balas Compradas',
+        'Aula1/Galinhas Compradas',
+        'Aula1/Batimentos Cardíacos',
+        
+    ]
+
+    listaDeLi = ""
+    contador = 0
+    while (contador < 10){
+        link = listaLink[contador]
+        listaDeLi = `<li><a href=${link}>${atividade[contador]}</a></li>`+listaDeLi
+        contador += 1
+
+    }
+
+    document.body.innerHTML =
+    `${htmlCertificado}
+    <ol>
+        ${listaDeLi}
+    </ol>
+    `
+    
+}
+
